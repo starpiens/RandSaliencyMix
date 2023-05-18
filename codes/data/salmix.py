379b4f37172774e2434b7f896f09a6b6f9dedcd9
Kim DongEon <90170785+DongEon31@users.noreply.github.com>
2023-05-18 17:49:07 +0900

    Update salmix.py

--- codes/data/salmix.py
+++ codes/data/salmix.py
@@ -2,10 +2,10 @@ import torch
 from torch.utils.data import Dataset
 import numpy as np
 import cv2
+from torchvision.utils import save_image
 
-
-def saliency_bbox(img, lam):
-    size = img.size()
+def saliency_bbox(inp, lam):
+    size = inp.size()
     width = size[1]
     height = size[2]
     cut_rat = np.sqrt(1. - lam)
@@ -13,7 +13,7 @@ def saliency_bbox(img, lam):
     cut_h = int(height * cut_rat)
 
     # initialize OpenCV's static fine grained saliency detector and compute the saliency map
-    temp_img = img.cpu().numpy().transpose(1, 2, 0)
+    temp_img = inp.cpu().numpy().transpose(1, 2, 0)
     saliency_detector = cv2.saliency.StaticSaliencyFineGrained_create()
     _, saliency_map = saliency_detector.computeSaliency(temp_img)
     saliency_map = (saliency_map * 255).astype("uint8")
@@ -26,20 +26,54 @@ def saliency_bbox(img, lam):
     bby1 = np.clip(y - cut_h // 2, 0, height)
     bbx2 = np.clip(x + cut_w // 2, 0, width)
     bby2 = np.clip(y + cut_h // 2, 0, height)
+    
+    # bbx1 ~ bbx2, bby1 ~ bby2 : Salient Region
+    Is = np.sum(saliency_map)
+    Ps = np.sum(saliency_map[bbx1 : bbx2, bby1 : bby2])
 
-    return bbx1, bby1, bbx2, bby2
+    return bbx1, bby1, bbx2, bby2, Is, Ps
+    
+
+def saliency_sum(inp_img, bbx1, bby1, bbx2, bby2, lam):
+    # Calculate Salient Region's Sum 
+    temp_img = inp_img.cpu().numpy().transpose(1, 2, 0)
+    saliency_detector = cv2.saliency.StaticSaliencyFineGrained_create()
+    _, saliency_map = saliency_detector.computeSaliency(temp_img)
+    saliency_map = (saliency_map * 255).astype("uint8")
+    It = np.sum(saliency_map)
+    
+    # Case : Sal to Corr
+    Pt = np.sum(saliency_map[bbx1 : bbx2, bby1 : bby2])
+
+    return It, Pt
 
 
 def saliency_mix(inp, tar, beta):
     # Generate mixed sample
     lam = np.random.beta(beta, beta)
     rand_index = torch.randperm(inp.shape[0])
-    tar_a = tar
-    tar_b = tar[rand_index]
-    bbx1, bby1, bbx2, bby2 = saliency_bbox(inp[rand_index[0]], lam)
+    tar_a = tar # inp의 label값 tar_a : target
+    tar_b = tar[rand_index] # inp의 label값 tar_a를 rand_index의 perm order대로 sort : source
+    bbx1, bby1, bbx2, bby2, Is, Ps = saliency_bbox(inp[rand_index[0]], lam)
+    
+    # Corr : Salient Region Sum
+    It, Pt = [], []
+    for inp_img in inp:
+        temp_It, temp_Pt = saliency_sum(inp_img, bbx1, bby1, bbx2, bby2, lam)
+        It.append(temp_It)
+        Pt.append(temp_Pt)
+
     inp[:, :, bbx1:bbx2, bby1:bby2] = inp[rand_index, :, bbx1:bbx2, bby1:bby2]
 
     # Adjust lambda to exactly match pixel ratio
     lam = 1 - ((bbx2 - bbx1) * (bby2 - bby1) / (inp.shape[-1] * inp.shape[-2]))
 
-    return inp, tar_a, tar_b, lam
+    # # augmented sample label Ya = Cs * Ys + Ct * Yt
+    Cs = Ps / Is
+    Ct = [1 - (Pt[i] / It[i]) for i in range(len(Pt))]
+
+    # normalization : Cs + Ct = 1
+    # norm_Cs = Cs / (Cs + Ct)
+    # norm_Ct = [Ct[i] / (Cs + Ct[i]) for i in range(len(Ct))]
+
+    return inp, tar_a, tar_b, Cs, Ct
