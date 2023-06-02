911914366e8cd7361e5c7c08fa74b3f10453da31
ohyoonju <61573404+ohyoonju@users.noreply.github.com>
2023-06-02 20:04:12 +0900

    Update ramdomly_selected_threshold_saliency_bbox

--- codes/data/salmix.py
+++ codes/data/salmix.py
@@ -107,6 +107,47 @@ def local_mean_saliency_bbox(img, lam):
         
     return f_bbx1, f_bby1, f_bbx2, f_bby2, K
 
+def ramdomly_selected_threshold_saliency_bbox(img, lam):
+    size = img.size()
+    width = size[1]
+    height = size[2]
+    cut_rat = np.sqrt(1. - lam)
+    cut_w = int(width * cut_rat)
+    cut_h = int(height * cut_rat)
+    
+    temp_img = img.cpu().numpy().transpose(1, 2, 0)
+    saliency_detector = cv2.saliency.StaticSaliencyFineGrained_create()
+    _, saliency_map = saliency_detector.computeSaliency(temp_img)
+    saliency_map = (saliency_map * 255).astype("uint8")
+
+    saliency_map_ = saliency_map
+    saliency_map_[:cut_w//2,:]= False
+    saliency_map_[:,:cut_h//2] = False
+    saliency_map_[width-cut_w//2:,:] = False
+    saliency_map_[:,height-cut_h//2:] = False
+
+    x_y_indices = np.where(saliency_map_)
+
+    threshold = 30
+    while True:
+        x_index = np.random.choice(len(x_y_indices[0]))
+        y_index = np.random.choice(len(x_y_indices[1]))
+        x = x_y_indices[0][x_index]
+        y = x_y_indices[1][y_index]
+        
+        bbx1 = np.clip(x - cut_w // 2, 0, width)
+        bby1 = np.clip(y - cut_h // 2, 0, height)
+        bbx2 = np.clip(x + cut_w // 2, 0, width)
+        bby2 = np.clip(y + cut_h // 2, 0, height)
+        
+        #print(bbx1,bbx2,bby1,bby2)
+        #if bbx1 >= bbx2 or bby1 >= bby2:
+        #    continue
+        sal_mean = saliency_map[bbx1:bbx2, bby1:bby2].mean()
+        if sal_mean > threshold:
+            return bbx1,bbx2,bby1,bby2
+
+
 def saliency_mix(inp, tar, beta):
     # Generate mixed sample
     lam = np.random.beta(beta, beta)
@@ -114,12 +155,13 @@ def saliency_mix(inp, tar, beta):
     tar_a = tar
     tar_b = tar[rand_index]
     #bbx1, bby1, bbx2, bby2 = saliency_bbox(inp[rand_index[0]], lam)
-    bbx1, bby1, bbx2, bby2 = threshold_rand_saliency_bbox(inp[rand_index[0]], lam)
-    bbx1, bby1, bbx2, bby2, K = local_mean_saliency_bbox(inp[rand_index[0]], lam)
+    #bbx1, bby1, bbx2, bby2 = threshold_rand_saliency_bbox(inp[rand_index[0]], lam)
+    #bbx1, bby1, bbx2, bby2, K = local_mean_saliency_bbox(inp[rand_index[0]], lam)
+    bbx1, bby1, bbx2, bby2 = ramdomly_selected_threshold_saliency_bbox(inp[rand_index[0]], lam)
     
     inp[:, :, bbx1:bbx2, bby1:bby2] = inp[rand_index, :, bbx1:bbx2, bby1:bby2]
 
     # Adjust lambda to exactly match pixel ratio
     lam = 1 - ((bbx2 - bbx1) * (bby2 - bby1) / (inp.shape[-1] * inp.shape[-2]))
 
-    return inp, tar_a, tar_b, lam, K
+    return inp, tar_a, tar_b, lam#, K
