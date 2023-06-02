bdcd3c7a2eb8c2f4eabdd3144b9cec4f7e8a3a3a
ohyoonju <61573404+ohyoonju@users.noreply.github.com>
2023-06-02 19:55:42 +0900

    Update threshold_rand_saliency_bbox

--- codes/data/salmix.py
+++ codes/data/salmix.py
@@ -17,14 +17,8 @@ def saliency_bbox(img, lam):
     saliency_detector = cv2.saliency.StaticSaliencyFineGrained_create()
     _, saliency_map = saliency_detector.computeSaliency(temp_img)
     saliency_map = (saliency_map * 255).astype("uint8")
-
-    threshold = 250
-    rand_saliency = saliency_map > threshold
-    true_indices = np.where(rand_saliency)
-    random_index = np.random.choice(len(true_indices[0]))
-    maximum_indices = (true_indices[0][random_index], true_indices[1][random_index])
-
-    #maximum_indices = np.unravel_index(np.argmax(saliency_map, axis=None), saliency_map.shape)
+    
+    maximum_indices = np.unravel_index(np.argmax(saliency_map, axis=None), saliency_map.shape)
     
     x = maximum_indices[0]
     y = maximum_indices[1]
@@ -36,6 +30,35 @@ def saliency_bbox(img, lam):
 
     return bbx1, bby1, bbx2, bby2
 
+def threshold_rand_saliency_bbox(img, lam):
+    size = img.size()
+    width = size[1]
+    height = size[2]
+    cut_rat = np.sqrt(1. - lam)
+    cut_w = int(width * cut_rat)
+    cut_h = int(height * cut_rat)
+
+    # initialize OpenCV's static fine grained saliency detector and compute the saliency map
+    temp_img = img.cpu().numpy().transpose(1, 2, 0)
+    saliency_detector = cv2.saliency.StaticSaliencyFineGrained_create()
+    _, saliency_map = saliency_detector.computeSaliency(temp_img)
+    saliency_map = (saliency_map * 255).astype("uint8")
+
+    threshold = 150
+    rand_saliency = saliency_map > threshold
+    true_indices = np.where(rand_saliency)
+    x_index = np.random.choice(len(true_indices[0]))
+    y_index = np.random.choice(len(true_indices[1]))
+    rand_indices = (true_indices[0][x_index], true_indices[1][y_index])
+    x = rand_indices[0]
+    y = rand_indices[1]
+
+    bbx1 = np.clip(x - cut_w // 2, 0, width)
+    bby1 = np.clip(y - cut_h // 2, 0, height)
+    bbx2 = np.clip(x + cut_w // 2, 0, width)
+    bby2 = np.clip(y + cut_h // 2, 0, height)
+
+    return bbx1, bby1, bbx2, bby2
 
 def saliency_mix(inp, tar, beta):
     # Generate mixed sample
@@ -43,7 +66,8 @@ def saliency_mix(inp, tar, beta):
     rand_index = torch.randperm(inp.shape[0])
     tar_a = tar
     tar_b = tar[rand_index]
-    bbx1, bby1, bbx2, bby2 = saliency_bbox(inp[rand_index[0]], lam)
+    #bbx1, bby1, bbx2, bby2 = saliency_bbox(inp[rand_index[0]], lam)
+    bbx1, bby1, bbx2, bby2 = threshold_rand_saliency_bbox(inp[rand_index[0]], lam)
     inp[:, :, bbx1:bbx2, bby1:bby2] = inp[rand_index, :, bbx1:bbx2, bby1:bby2]
 
     # Adjust lambda to exactly match pixel ratio
